019acfea1c5abc7b1d67abf8f123083492d6e914
userhv <heliovictor05@hotmail.com>
2020-03-10 11:32:47 -0300

    version 0.9.beta.8

--- relacao.c
+++ relacao.c
@@ -9,6 +9,7 @@
     - Corrigir a função "transitiva" e sua impressão no main
     - O algoritmo precisa ser otimizado
     - O algoritmo precisa ser sintetizado
+    - beta.7
 */
 
 int** aloc_int(int num_ln)
